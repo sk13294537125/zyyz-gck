f30e7b493be4a4c954f9f7bdb1bc6a1b4d0a5e56
ext.shikai1 <ext.shikai1@jd.com>
2024-05-20 11:09:51 +0800

    -- init

---
+++ gck-quartz/src/main/java/com/gc/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.gc.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import com.gc.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
